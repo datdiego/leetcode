d39efbf9b458c1373ce608a21ea50af09cb0a2d5
diego alducin <diego.alducin@gmail.com>
2022-05-20 18:00:29 -0500

    Create even_digits.py

---
+++ Easy/Even number of Digits/even_digits.py
@@ -0,0 +1,20 @@
+
+class Solution(object):
+    def findNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        even_digits = 0
+        
+        for num in nums:
+            digits = 0
+            while num > 0:
+                digits += 1
+                num /= 10
+                
+            
+            if digits % 2 == 0:
+                even_digits += 1
+            
+        return 
