b36765d42c5310b0cd90607d4eeefdbc8deed3cd
diego alducin <diego.alducin@gmail.com>
2022-05-23 20:42:40 -0500

    Create duplicate_zeroes.java

---
+++ Easy/1089 - Duplicate Zeroes/duplicate_zeroes.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        
+        int length = arr.length;
+        
+        for(int i = 0; i < length; i++){
+            
+            if(arr[i] == 0 && i != length-1){
+                
+                for(int j = length-2; j >= i; j--){
+                    
+                    arr[j+1] = arr[j];
+
+                }
+
+               arr[i+1] = 0;
+                i++; 
+              
+            }
+            
+        }
+    }
+}
