dc9be051190900424fd2d66c2b9440e3b1c5ddae
diego alducin <diego.alducin@gmail.com>
2022-10-03 10:45:10 -0500

    Create remove_element.py

---
+++ 27 - Remove Element/remove_element.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        count=nums.count(val)
+        for i in range(count):
+            nums.remove(val)
